d4868ca1e9dc1c0a61cea308f326c58df550c0f5
Corey <gipsonc@gmatc.matc.edu>
2022-02-14 12:32:20 -0600

    Completed Activity 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        GoodThings
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000')
+    .then((response)=> {
+      this.setState({
+        data: response.data
+      })
+    })
+    .catch((error)=> {
+      console.error(`Error fetching data: ${error}`);
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          GoodThings
+        </header>
+        {this.state.data}
+      </div>
+    );
+  }
+
 }
+
+
 
 export default App;
